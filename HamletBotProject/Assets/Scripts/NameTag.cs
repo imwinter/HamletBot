5998c1d82b085783d03d7e5a269543cd1409f570
Ian Winter <iwinter@ucsc.edu>
2014-07-20 20:27:16 -0700

    name tags now rotate to face player

---
+++ HamletBotProject/Assets/Scripts/NameTag.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NameTag : MonoBehaviour {
+
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target);
+		transform.Rotate(Vector3.up*180);
+	}
+}
